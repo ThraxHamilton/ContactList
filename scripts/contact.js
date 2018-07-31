e70a0eb45fde02819eb399f5e24aabb3bc8d77aa
ThraxHamilton <mmcclenton16@gmail.com>
2018-07-31 10:26:29 -0500

    two

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,7 +1,8 @@
+'use strict'
 function createContact(name, phone, address){
     return `<h1>${name}</h1>
-            <p>${phone}
-            <p>${address}`;
+            <p>${phone}</p>
+            <p>${address}</p>`;
 }
 
 module.exports = createContact;
--- scripts/contactList.js
+++ scripts/contactList.js
@@ -1,3 +1,4 @@
+'use strict'
 let contact = require('./contact');
 let contactCollection = require('./contactCollection');
 
@@ -6,3 +7,16 @@ contactCollection().forEach(contact =>
     createContact(contact.name, contact.phone, contact.address)
 )
 }
+
+function listContacts() {
+    getContacts().forEach(contact =>{
+        let contactComponent = createContact(contact.name, contact.phone, contact.address);
+        writeContactToDom(contactComponent);
+    });
+}
+
+function writeContactToDom(contact){
+    document.querySelector('#contactList').innerHTML += contact
+}
+
+module.exports = listContacts;
